188211c72e759c12300e5b7676f214e2a345fcf0
WillRRRR <30376565+WillRRRR@users.noreply.github.com>
2021-04-29 08:51:18 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<h2>Contact Us</h2>
+<ul>
+	<li>JNA (Apple devices only): +1 (405) 615-2605</li>
+	<li>Sheila: +1 (405) 615-2604</li>
+</ul>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<?php echo '
+<nav>
+	<ul>
+		<li><a href="archer.php">Archer family</a></li>
+		<li><a href="archer-phone.php">Archer iPhones</a></li>
+	</ul>
+</nav>';
+?>
